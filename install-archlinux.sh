a8496a4b945279c94d56198445e5de06d7019587
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-09-19 17:31:07 +0700

    Update install-archlinux.sh

--- install-archlinux.sh
+++ install-archlinux.sh
@@ -35,23 +35,6 @@ systemctl daemon-reload
 systemctl enable logitechmediaserver-git.service
 systemctl restart logitechmediaserver-git.service
 
-
-echo "Do you want to download and install Plugins? (Yes/No)"
-read answer_plugins
-if [ "$answer_plugins" = "Yes" ]; then
-
-	echo "Download Plugins and Install Plugin"
-	wget https://raw.githubusercontent.com/lovehifi/raudiolms-32bit/main/material-menu.tgz
-	wget https://raw.githubusercontent.com/lovehifi/raudiolms-32bit/main/plugin-data.tgz
-	wget https://raw.githubusercontent.com/lovehifi/raudiolms-32bit/main/plugin-data.yaml.tgz
-	wget https://raw.githubusercontent.com/lovehifi/raudiolms-32bit/main/plugin-prefs.tgz
-	tar -xzf material-menu.tgz --overwrite -C /opt/logitechmediaserver-git/prefs/
-	tar -xzf plugin-data.yaml.tgz --overwrite -C /opt/logitechmediaserver-git/cache/
-	tar -xzf plugin-prefs.tgz --overwrite -C /opt/logitechmediaserver-git/prefs/
-	tar -xzf plugin-data.tgz --overwrite -C /opt/logitechmediaserver-git/cache/InstalledPlugins/Plugins/
-fi
-
-
 echo "Do you want to download and install Squeezelite? (Yes/No)"
 read answer_sq
 if [ "$answer_sq" = "Yes" ]; then
