d5eae4b350382510c0017f24524a6b986e70a0e1
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-10-06 09:48:18 +0700

    Add files via upload

---
+++ sq.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Extract the card number from rAudio
+card=$(grep -oP '(?<=defaults.pcm.card ).*' /etc/asound.conf)
+echo "Card number: $card"
+
+/opt/sq/squeezelite32 -o hw:$card -n SQ32-rAudio -s 127.0.0.1 -m 00:00:00:00:00:00
+
